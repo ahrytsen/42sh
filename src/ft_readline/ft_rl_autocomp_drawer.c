57a942d3ddbafdbda904f73020cb776299130043
CosmicSpaceGoose <dim13@i.ua>
2018-09-06 13:35:01 +0300

    fix_autocomp

--- src/ft_readline/ft_rl_autocomp_drawer.c
+++ src/ft_readline/ft_rl_autocomp_drawer.c
@@ -27,37 +27,41 @@ static t_list	*rl_get_list_elem(t_list *lst, int row, int col, int num_row)
 	return (lst);
 }
 
-static void		rl_drawing_cycle(char **win_buf, t_list *lst, int *dim, int num)
+/*
+**	i[]: 0 - column, 1 - row, 2 - count
+*/
+
+static void		rl_drawing_cycle(char **buf, t_list *lst, int *dim)
 {
-	int		count;
-	int		col;
-	int		row;
+	int		i[3];
 	t_list	*elem;
 
-	count = 1;
-	row = 1;
+	i[2] = 1;
+	i[1] = 1;
 	write(0, "\n", 1);
 	tputs(tgetstr("cd", 0), 1, term_print);
-	while (count <= num)
+	while (i[2] <= dim[3])
 	{
-		col = 0;
-		while ((elem = rl_get_list_elem(lst, row, col, dim[2])))
+		i[0] = 0;
+		while ((elem = rl_get_list_elem(lst, i[1], i[0], dim[2])))
 		{
-			ft_strcat(*win_buf, (char *)elem->content);
-			if (col + 1 != dim[1])
-				ft_memset((void *)(*win_buf + ft_strlen(*win_buf)), ' ',
-					dim[0] - (int)elem->content_size + 1);
-			count++;
-			col++;
+			ft_strcat(*buf, (char *)elem->content);
+			(i[0] + 1 != dim[1]) ? ft_memset((void *)(*buf + ft_strlen(*buf))
+				, ' ', dim[0] - (int)elem->content_size + 1) : 0;
+			i[2]++;
+			i[0]++;
 		}
-		ft_strcat(*win_buf, "\n");
-		col + 1 == dim[1] ? (dim[1]--) : 0;
-		row++;
+		ft_strcat(*buf, "\n");
+		i[0] + 1 == dim[1] ? (dim[1]--) : 0;
+		i[1]++;
 	}
+	i[0] = get_term()->cury + 1;
+	while (--i[0])
+		ft_strcat(*buf, "\n");
 }
 
 /*
-**	dim[] - dimensions: 0 - max name, 1 - columns, 2 - rows, 3 - list size
+**	dim[]: 0 - max name, 1 - columns, 2 - rows, 3 - list size
 */
 
 static void		rl_get_dimensions(int *dim)
@@ -104,7 +108,7 @@ static int		rl_get_max_name(t_list *lst)
 char			*ft_rl_match_drawer(t_list *lst, char *str)
 {
 	int		dim[4];
-	char	*win_buf;
+	char	*buf;
 	char	*ptr;
 
 	ptr = NULL;
@@ -116,11 +120,11 @@ char			*ft_rl_match_drawer(t_list *lst, char *str)
 	else if (get_term()->comp_stage == -1)
 	{
 		dim[0] = rl_get_max_name(lst);
-		win_buf = (char *)ft_memalloc((dim[0] + 1) * dim[3]);
+		buf = (char *)ft_memalloc((dim[0] + 1) * dim[3] + get_term()->cury + 1);
 		rl_get_dimensions(dim);
-		rl_drawing_cycle(&win_buf, lst, dim, (int)dim[3]);
-		write(0, win_buf, (size_t)dim[0] * dim[3]);
-		free(win_buf);
+		rl_drawing_cycle(&buf, lst, dim);
+		ft_putstr_fd(buf, 0);
+		free(buf);
 		ft_redraw_line();
 		get_term()->comp_stage = 0;
 	}
